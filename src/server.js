c245cf65e2889f9747817ed1eee8c4a789001193
Okjoo Han <okjoo.han88@gmail.com>
2023-02-27 22:14:57 +0900

    set first server using Express.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send("<h1>Don't think. Just do!</h1>");
+};
+const handleLogin = (req, res) => {
+    return res.send({ message: "Login here." });
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+    console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
