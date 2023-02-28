842fc393a2a1de861c886883b91d9129b5eedf06
Okjoo Han <okjoo.han88@gmail.com>
2023-02-28 13:41:20 +0900

    middleware 사용해보기

--- src/server.js
+++ src/server.js
@@ -4,14 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to ${req.url}`);
+    next();
+};
+
 const handleHome = (req, res) => {
     return res.send("<h1>Don't think. Just do!</h1>");
 };
-const handleLogin = (req, res) => {
-    return res.send({ message: "Login here." });
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
