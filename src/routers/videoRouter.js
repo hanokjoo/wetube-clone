dabd3af1bc68ab3067d918d864e3e44d47ad048b
Okjoo Han <okjoo.han88@gmail.com>
2023-03-07 22:06:56 +0900

    pug 설치, view engine, views 설정

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+    watch,
+    edit,
+    upload,
+    deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,7 +12,7 @@ const videoRouter = express.Router();
 /:id가 /upload 보다 위에 있으면 "upload" 문자를 id값으로 받아들인다.
 숫자만 입력받는 정규식을 추가하면 "upload"를 id값으로 받아들이지 않으므로 잘 동작함.
 */
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
