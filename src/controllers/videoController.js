b88b8f066940e4e6afda231d2b5e0adf2ac0e336
Okjoo Han <okjoo.han88@gmail.com>
2023-03-07 23:05:54 +0900

    template inheritance(상속) block, extends 사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.render("home");
-export const edit = (req, res) => res.send("Edit Videos");
+export const edit = (req, res) => res.render("edit");
 export const watch = (req, res) => res.render("watch");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload Video");
