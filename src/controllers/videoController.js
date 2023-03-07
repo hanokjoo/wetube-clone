e86d210202af3f49b347955e6310faeb3ee49afc
Okjoo Han <okjoo.han88@gmail.com>
2023-03-07 16:38:53 +0900

    router, controller 추가, url에 ':변수(+정규식)' 넣기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,9 @@
 export const trending = (req, res) => res.send("Home Page Videos");
 export const edit = (req, res) => res.send("Edit Videos");
-export const watch = (req, res) => res.send("Watch Videos");
+export const see = (req, res) => res.send("See Videos");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload Video");
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    res.send("Delete Video");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { edit, watch } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+/* 
+/:id가 /upload 보다 위에 있으면 "upload" 문자를 id값으로 받아들인다.
+숫자만 입력받는 정규식을 추가하면 "upload"를 id값으로 받아들이지 않으므로 잘 동작함.
+*/
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
