1a90311e999bae2e75467abcfceb654aeb2c5e4e
Okjoo Han <okjoo.han88@gmail.com>
2023-03-21 16:20:03 +0900

    db에 저장하는 방법1: js object 생성해서 save()로 저장

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,12 +11,9 @@ export const home = (req, res) => {
 };
 */
 export const home = async (req, res) => {
-    try {
-        const videos = await Video.find({});
-        return res.render("home", { pageTitle: "Home", videos });
-    } catch {
-        return res.render("server-error", { error });
-    }
+    const videos = await Video.find({});
+    console.log(videos);
+    return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
     const { id } = req.params;
@@ -34,8 +31,18 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-    // add a video to the videos array.
-    const { title } = req.body;
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    const video = new Video({
+        title,
+        description,
+        createdAt: Date.now(),
+        hashtags: hashtags.split(",").map((word) => `#${word.trim()}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+    });
+    await video.save();
     return res.redirect("/");
 };
