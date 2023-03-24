87a35d0c9215cdd98edb0efe176786a3fc063a9a
Okjoo Han <okjoo.han88@gmail.com>
2023-03-24 22:08:32 +0900

    middleware -> static 적용해보기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,5 @@
 import Video from "../models/Video";
 
-/* callback 버전
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => {
-        if (error) {
-            return res.render("server-error");
-        }
-        return res.render("home", { pageTitle: "Home", videos });
-    });
-};
-*/
 export const home = async (req, res) => {
     const videos = await Video.find({});
     console.log(videos);
@@ -41,9 +31,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title,
         description,
-        hashtags: hashtags
-            .split(",")
-            .map((word) => (word.startsWith("#") ? word : `#${word.trim()}`)),
+        hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect(`/videos/${id}`);
 };
@@ -57,7 +45,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags,
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     } catch (error) {
