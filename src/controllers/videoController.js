a45eb546c3b2b4769309951b977d14df70aef553
Okjoo Han <okjoo.han88@gmail.com>
2023-04-06 21:42:32 +0900

    Join, Video upload/edit 실패 시 status code 보내기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,17 +16,17 @@ export const watch = async (req, res) => {
 export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    console.log(video);
-
+    if (!video) {
+        return res.status(404).render("404", { pageTitle: "Video not found." });
+    }
     return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 export const postEdit = async (req, res) => {
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
     const video = await Video.exists({ _id: id });
-    console.log(video);
     if (!video) {
-        return res.render("404", { pageTitle: "Video not found." });
+        return res.status(404).render("404", { pageTitle: "Video not found." });
     }
     await Video.findByIdAndUpdate(id, {
         title,
@@ -49,7 +49,7 @@ export const postUpload = async (req, res) => {
         });
         return res.redirect("/");
     } catch (error) {
-        return res.render("upload", {
+        return res.status(400).render("upload", {
             pageTitle: "Upload Video",
             errorMessage: error._message,
         });
