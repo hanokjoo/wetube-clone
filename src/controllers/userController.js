3c92fa33907f5857507592b4ff5c01c4dc1789ab
Okjoo Han <okjoo.han88@gmail.com>
2023-05-04 22:53:37 +0900

    change_password_1: view, controller 생성. socialOnly: true인 user는 보이지않게 처리

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -181,4 +181,15 @@ export const postEdit = async (req, res) => {
     }; */
     return res.redirect("/users/edit");
 };
+export const getChangePassword = (req, res) => {
+    if (req.session.user.socialOnly) {
+        return res.redirect("/");
+    }
+    return res.render("users/change-password", {
+        pageTitle: "Change Password",
+    });
+};
+export const postChagePassword = (req, res) => {
+    return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
