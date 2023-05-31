ee1a15f373e38c0aa05058fb4e2e82a77344a42f
Okjoo Han <okjoo.han88@gmail.com>
2023-05-31 22:53:52 +0900

    volume: volume range로 조절하는 이벤트 생성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
     if (video.paused) {
         video.play();
@@ -20,8 +23,32 @@ const handleMute = (e) => {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
+    video.volume = volumeRange.value;
+};
+
+const handleChangeVolumeRange = (e) => {
+    const {
+        target: { value },
+    } = e;
+    if (Number(value) === 0) {
+        video.muted = true;
+        muteBtn.innerText = "Unmute";
+    } else volumeValue = value;
+};
+
+const handleInputVolumeRange = (e) => {
+    const {
+        target: { value },
+    } = e;
+    if (video.muted) {
+        video.muted = false;
+    }
+    muteBtn.innerText = video.muted ? "Unmuted" : "Mute";
+    video.volume = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleInputVolumeRange);
+volumeRange.addEventListener("change", handleChangeVolumeRange);
