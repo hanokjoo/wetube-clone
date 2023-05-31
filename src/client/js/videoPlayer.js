deeca7bbef88465900b0dd6a3d8ab650c7e31080
Okjoo Han <okjoo.han88@gmail.com>
2023-05-31 21:17:36 +0900

    play_pause: play/pause 버튼 이벤트 생성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+const handelMute = (e) => {};
+
+const handlePause = (e) => (playBtn.innerText = "Play");
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handelMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
