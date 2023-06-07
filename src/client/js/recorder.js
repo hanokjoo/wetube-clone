d6e30e760e3b25888d94d0f5ee0cc947dadb78b8
Okjoo Han <okjoo.han88@gmail.com>
2023-06-07 22:06:22 +0900

    downloading_the_file: 녹화된 비디오 다운로드하기

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,7 +1,7 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-let stream, recorder;
+let stream, recorder, videoFile;
 
 const init = async () => {
     stream = await navigator.mediaDevices.getUserMedia({
@@ -17,10 +17,10 @@ const handleStartRecording = () => {
     startBtn.removeEventListener("click", handleStartRecording);
     startBtn.addEventListener("click", handleStopRecording);
 
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
         // 녹화한 영상파일(브라우저의 메모리)을 가르키는 URL, 서버엔 없음
-        const videoFile = URL.createObjectURL(event.data);
+        videoFile = URL.createObjectURL(event.data);
         video.srcObject = null;
         video.src = videoFile;
         video.loop = true;
@@ -34,7 +34,13 @@ const handleStopRecording = () => {
     startBtn.addEventListener("click", handleDownload);
     recorder.stop();
 };
-const handleDownload = () => {};
+const handleDownload = () => {
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
+};
 
 init();
 startBtn.addEventListener("click", handleStartRecording);
