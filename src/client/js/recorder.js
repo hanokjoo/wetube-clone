d9ddf6a23d894de36712b29d1fb3ccdf04c660f9
Okjoo Han <okjoo.han88@gmail.com>
2023-06-10 23:50:08 +0900

    thumbnail_upload_1: 다운로드 후 재녹화 할 수 있게 수정, Video 모델에 thumbUrl 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,18 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream, recorder, videoFile;
 
+const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg",
+};
+
 const init = async () => {
-    startBtn.innerText = "Start Recording";
+    actionBtn.innerText = "Start Recording";
     stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: true,
@@ -16,9 +22,9 @@ const init = async () => {
 };
 
 const handleStartRecording = () => {
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", handleStartRecording);
-    startBtn.addEventListener("click", handleStopRecording);
+    actionBtn.innerText = "Stop Recording";
+    actionBtn.removeEventListener("click", handleStartRecording);
+    actionBtn.addEventListener("click", handleStopRecording);
 
     recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
@@ -32,29 +38,33 @@ const handleStartRecording = () => {
     recorder.start();
 };
 const handleStopRecording = () => {
-    startBtn.innerText = "Download Recording";
-    startBtn.removeEventListener("click", handleStopRecording);
-    startBtn.addEventListener("click", handleDownload);
+    actionBtn.innerText = "Download Recording";
+    actionBtn.removeEventListener("click", handleStopRecording);
+    actionBtn.addEventListener("click", handleDownload);
     recorder.stop();
 };
 const handleDownload = async () => {
+    actionBtn.removeEventListener("click", handleDownload);
+    actionBtn.innerText = "Transcoding...";
+    actionBtn.disabled = true;
+
     const ffmpeg = createFFmpeg({ log: true });
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // recording.webm -> output.mp4(초당 60프레임)로 변환
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+    await ffmpeg.run("-i", files.input, "-r", "60", files.output); // files.input -> files.output(초당 60프레임)로 변환
     await ffmpeg.run(
         "-i",
-        "recording.webm",
+        files.input,
         "-ss",
         "00:00:01",
         "-frames:v",
         "1",
-        "thumbnail.jpg"
+        files.thumb
     );
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
-    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+    const mp4File = ffmpeg.FS("readFile", files.output);
+    const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
     const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
     const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
@@ -62,33 +72,38 @@ const handleDownload = async () => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
-    const a = document.createElement("a");
-    a.href = mp4Url;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
+    downloadFile(mp4Url, "MyRecording.mp4");
+    downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-    const thumbA = document.createElement("a");
-    thumbA.href = thumbUrl;
-    thumbA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
-
-    ffmpeg.FS("unlink", "recording.webm");
-    ffmpeg.FS("unlink", "output.mp4");
-    ffmpeg.FS("unlink", "thumbnail.jpg");
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", files.output);
+    ffmpeg.FS("unlink", files.thumb);
 
     URL.revokeObjectURL(mp4Url);
     URL.revokeObjectURL(thumbUrl);
     URL.revokeObjectURL(videoFile);
 
+    /*
     video.pause();
     video.src = "";
     document.body.removeChild(a);
     document.body.removeChild(thumbA);
     stream.removeTrack(stream.getTracks()[0]);
+    */
+
+    actionBtn.disabled = false;
     init();
+    actionBtn.innerText = "Record Again";
+    actionBtn.addEventListener("click", handleStartRecording);
+};
+
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
 };
 
 init();
-startBtn.addEventListener("click", handleStartRecording);
+actionBtn.addEventListener("click", handleStartRecording);
