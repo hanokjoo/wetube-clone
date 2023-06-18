6b0f1383ff32913e2cf5e0853f85f1432a99dbc9
Okjoo Han <okjoo.han88@gmail.com>
2023-06-18 20:47:11 +0900

    realtime_comments: 새로운 댓글element 만들어서 리스트에 추가하기

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,19 +1,34 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
     const textarea = form.querySelector("textarea");
     const btn = form.querySelector("button");
     const text = textarea.value;
     const videoId = videoContainer.dataset.videoid;
     if (text.trim() === "") return;
-    fetch(`/api/videos/${videoId}/comment`, {
+    const { status } = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text }),
     });
     textarea.value = "";
+    if (status === 201) {
+        addComment(text);
+    }
+};
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
 };
 
 if (form) {
